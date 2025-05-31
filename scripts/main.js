b49ef5e0214ccc3540cc59762bfb634390ca42bd
PorlHoof <kar3nik.github@gmail.com>
2025-05-31 13:44:28 +0300

    создал общий класс и фаер-мага

---
+++ scripts/main.js
@@ -0,0 +1,87 @@
+// пробую використовувати ООП
+class RandomUtils {
+  static getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  static getRandomFloat(min, max, decimals = 2) {
+    const num = Math.random() * (max - min) + min;
+    return parseFloat(num.toFixed(decimals));
+  }
+
+  static shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
+}
+
+class Hero {
+  constructor() {
+    this.health = 100;
+    this.mana = 50;
+  }
+
+  userEscape(min, max, factor) {
+    if (RandomUtils.getRandomInt(min, max) > factor) {
+      console.log("Ви втекли");
+    } else {
+      console.log("Ви намагались втекти але вас вбило");
+    }
+  }
+
+  userHit(hit) {
+    if (this.mana > 150) {
+      console.log("Максимальне значення мани");
+      return;
+    }
+    this.mana += this.manaRegeneration;
+    console.log(hit);
+  }
+
+  userPower(skill) {
+    if (this.mana < this.manaCost) {
+      console.log("Недостатньо мани");
+      return;
+    }
+    this.mana -= this.manaCost;
+    console.log(skill);
+  }
+
+  showStats() {
+    console.log(
+      `Герой ${this.element} рівень ХП: ${this.health}, рівень мани: ${this.mana}`
+    );
+  }
+}
+
+class Mage extends Hero {
+  constructor() {
+    super();
+    this.element = "Маг вогню";
+    this.mana += 50; // Хочу добавить больше стихий
+    this.health -= 25;
+    this.manaCost = 50;
+    this.manaRegeneration = 50;
+  }
+
+  userPower() {
+    super.userPower("FIREBALL");
+  }
+
+  userHit() {
+    super.userHit("Удар палицею");
+  }
+
+  userEscape() {
+    super.userEscape(1, 10, 7);
+  }
+}
+
+// class Barbarian extends Hero {} планирую добовление класса варвар
+
+const fireMage = new Mage();
+
+fireMage.userEscape();
